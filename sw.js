352f6a04b99d1e40f3a4dd1358e278661c15c0a9
alexandros409 <alexandroschatziliadis@gmail.com>
2026-07-11 03:13:22 +0200

    Implement service worker event listeners
    Add event listeners for install, activate, and fetch in service worker.

--- sw.js
+++ sw.js
@@ -1,1 +1,11 @@
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
 
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  // Απαραίτητο fetch event για να εγκρίνει ο Chrome την εγκατάσταση
+});
